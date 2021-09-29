5a6a5144c27f206eb704c5b62e2b4f41a69cbf5a
libertus-codelin <91585349+libertus-codelin@users.noreply.github.com>
2021-09-29 13:46:52 +0700

    Membuat dan menyelesaikan soal variabel pada php

---
+++ variabel-latihan.php
@@ -0,0 +1,15 @@
+<?php 
+
+   /* contoh penggunaan variabel  */
+
+   $uangSaya = 15000;
+   $hargaAyam = 10000;
+   $uangKembalian = ($uangSaya - $hargaAyam);
+
+   echo "Uang yg saya punya sebesar <b>Rp. ".$uangSaya."</b>";
+   echo "<br/>";
+   echo "Saya membeli ayam sekilonya sebesar <b>Rp. ".$hargaAyam."</b>";
+   echo "<br/>";
+   echo "Jadi sisa uang saya adalah sebesar <b>Rp. ".$uangKembalian."</b>";
+
+?>
---
+++ variabel.php
@@ -0,0 +1,16 @@
+<?php 
+
+   /* cara membuat sebuah variabel untuk menampung nama dan umur.
+      Penjelasan:
+      - untuk membuat variabel, harus diawali dengan simbol dollar ($) didepannya lalu diikuti dengan nama variabel yang kita inginkan namun sesuai dengan apa yang akan kita tampilkan nantinya, contohnya sebagai berikut:
+   */
+
+   $nama = "Libertus";
+   $umur = 21;
+   echo "Nama : ". $nama;
+   echo "<br/>";
+   echo "Umur : ".$umur." tahun";
+   echo "<br/>";
+   echo "Halo, perkenalkan nama saya ".$nama." dan umur saya ".$umur." tahun";
+
+?>
